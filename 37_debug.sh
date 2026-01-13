cb49b57c55fe3cbb4d7c13c235450988fed8fab0
panditmayur888 <panditmayur888@gmail.com>
2026-01-13 16:35:03 +0530

    shell_scripts

--- 37_debug.sh
+++ 37_debug.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-set -x 
-#To access the arguments
+set -e
 
-echo "First argument is $1"
-echo "Second argument is $2"
+pwd
+date
+cd /home/vaibhav
+hostname
 
-echo "All the arguments are - $@"
-echo "Number of arguments are - $#"
